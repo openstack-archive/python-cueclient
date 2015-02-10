1dbc18c87bb9dd706ca1594c3d850a0266eb275b
Abitha Palaniappan <abitha.palaniappan@hp.com>
2015-02-10 15:41:18 -0800

    Minor changes to Cluster create and list functions
    Change-Id: Ica21e5331b77a0252370a9d5527b62907a2963c2

--- cueclient/controller.py
+++ cueclient/controller.py
@@ -6,7 +6,7 @@
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -22,7 +22,6 @@ import six
 
 @six.add_metaclass(abc.ABCMeta)
 class Controller(object):
-
     def __init__(self, client):
         self.client = client
 
@@ -88,7 +87,6 @@ class Controller(object):
 
 @six.add_metaclass(abc.ABCMeta)
 class CrudController(Controller):
-
     @abc.abstractmethod
     def list(self, *args, **kw):
         """
