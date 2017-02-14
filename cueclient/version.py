43e19247716cf6b088dbd1d79fe33162c933ce52
Jeremy Liu <liujiong@gohighsec.com>
2017-02-14 17:18:17 +0800

    Add version info to cueclient module
    According to PEP 396, the version of client library
    should be set in xxxclient.__version__, that help
    python-openstackclient command "module list" to get
    the right plugin module versions.
    
    Change-Id: Ic255c135d6a1a611e60b42291568ba1c453ab5dc
    Related-Bug: #1662058

--- cueclient/version.py
+++ cueclient/version.py
@@ -13,6 +13,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-import pbr.version
+import cueclient
 
-version_info = pbr.version.VersionInfo('python-cueclient')
+version_info = cueclient.version_info
